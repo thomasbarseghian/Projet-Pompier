639c356bd07cd2aa200de9680024f194f6b06ef2
abdullah <abdullah.nezami@etu.unistra.fr>
2025-05-08 18:00:02 +0200

    Sidebar completed

---
+++ Barseghian_Nezami_SAE25/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿using System.Drawing;
+
+namespace Barseghian_Nezami_SAE25
+{
+    partial class mainLayout
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainLayout));
+            this.pnlSideBar = new System.Windows.Forms.Panel();
+            this.btnGestionEngins = new System.Windows.Forms.Button();
+            this.btnGestionPersonnel = new System.Windows.Forms.Button();
+            this.btnStatistiques = new System.Windows.Forms.Button();
+            this.btnNouvelleMission = new System.Windows.Forms.Button();
+            this.btnTableauBord = new System.Windows.Forms.Button();
+            this.btnQuitter = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.pnlSideBar.SuspendLayout();
+            this.btnQuitter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlSideBar
+            // 
+            this.pnlSideBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(30)))), ((int)(((byte)(40)))));
+            this.pnlSideBar.Controls.Add(this.btnGestionEngins);
+            this.pnlSideBar.Controls.Add(this.btnGestionPersonnel);
+            this.pnlSideBar.Controls.Add(this.btnStatistiques);
+            this.pnlSideBar.Controls.Add(this.btnNouvelleMission);
+            this.pnlSideBar.Controls.Add(this.btnTableauBord);
+            this.pnlSideBar.Controls.Add(this.btnQuitter);
+            this.pnlSideBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlSideBar.Name = "pnlSideBar";
+            this.pnlSideBar.Size = new System.Drawing.Size(301, 755);
+            this.pnlSideBar.TabIndex = 0;
+            // 
+            // btnGestionEngins
+            // 
+            this.btnGestionEngins.AutoSize = true;
+            this.btnGestionEngins.BackColor = System.Drawing.Color.Transparent;
+            this.btnGestionEngins.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnGestionEngins.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGestionEngins.FlatAppearance.BorderSize = 0;
+            this.btnGestionEngins.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnGestionEngins.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
+            this.btnGestionEngins.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGestionEngins.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGestionEngins.ForeColor = System.Drawing.Color.White;
+            this.btnGestionEngins.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGestionEngins.Location = new System.Drawing.Point(0, 184);
+            this.btnGestionEngins.Name = "btnGestionEngins";
+            this.btnGestionEngins.Size = new System.Drawing.Size(300, 50);
+            this.btnGestionEngins.TabIndex = 10;
+            this.btnGestionEngins.Text = "   Gestion des Engins";
+            this.btnGestionEngins.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnGestionEngins.UseVisualStyleBackColor = false;
+            // 
+            // btnGestionPersonnel
+            // 
+            this.btnGestionPersonnel.AutoSize = true;
+            this.btnGestionPersonnel.BackColor = System.Drawing.Color.Transparent;
+            this.btnGestionPersonnel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnGestionPersonnel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGestionPersonnel.FlatAppearance.BorderSize = 0;
+            this.btnGestionPersonnel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnGestionPersonnel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
+            this.btnGestionPersonnel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGestionPersonnel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGestionPersonnel.ForeColor = System.Drawing.Color.White;
+            this.btnGestionPersonnel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGestionPersonnel.Location = new System.Drawing.Point(0, 254);
+            this.btnGestionPersonnel.Name = "btnGestionPersonnel";
+            this.btnGestionPersonnel.Size = new System.Drawing.Size(300, 50);
+            this.btnGestionPersonnel.TabIndex = 9;
+            this.btnGestionPersonnel.Text = "   Ressources humaines";
+            this.btnGestionPersonnel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnGestionPersonnel.UseVisualStyleBackColor = false;
+            // 
+            // btnStatistiques
+            // 
+            this.btnStatistiques.AutoSize = true;
+            this.btnStatistiques.BackColor = System.Drawing.Color.Transparent;
+            this.btnStatistiques.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnStatistiques.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStatistiques.FlatAppearance.BorderSize = 0;
+            this.btnStatistiques.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnStatistiques.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
+            this.btnStatistiques.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStatistiques.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStatistiques.ForeColor = System.Drawing.Color.White;
+            this.btnStatistiques.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStatistiques.Location = new System.Drawing.Point(0, 324);
+            this.btnStatistiques.Name = "btnStatistiques";
+            this.btnStatistiques.Size = new System.Drawing.Size(300, 50);
+            this.btnStatistiques.TabIndex = 8;
+            this.btnStatistiques.Text = "   Statistiques";
+            this.btnStatistiques.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnStatistiques.UseVisualStyleBackColor = false;
+            // 
+            // btnNouvelleMission
+            // 
+            this.btnNouvelleMission.AutoSize = true;
+            this.btnNouvelleMission.BackColor = System.Drawing.Color.Transparent;
+            this.btnNouvelleMission.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNouvelleMission.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNouvelleMission.FlatAppearance.BorderSize = 0;
+            this.btnNouvelleMission.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnNouvelleMission.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
+            this.btnNouvelleMission.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNouvelleMission.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNouvelleMission.ForeColor = System.Drawing.Color.White;
+            this.btnNouvelleMission.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNouvelleMission.Location = new System.Drawing.Point(0, 114);
+            this.btnNouvelleMission.Name = "btnNouvelleMission";
+            this.btnNouvelleMission.Size = new System.Drawing.Size(300, 50);
+            this.btnNouvelleMission.TabIndex = 7;
+            this.btnNouvelleMission.Text = "   Nouvelle Mission";
+            this.btnNouvelleMission.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNouvelleMission.UseVisualStyleBackColor = false;
+            // 
+            // btnTableauBord
+            // 
+            this.btnTableauBord.AutoSize = true;
+            this.btnTableauBord.BackColor = System.Drawing.Color.Transparent;
+            this.btnTableauBord.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnTableauBord.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTableauBord.FlatAppearance.BorderSize = 0;
+            this.btnTableauBord.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnTableauBord.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
+            this.btnTableauBord.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTableauBord.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTableauBord.ForeColor = System.Drawing.Color.White;
+            this.btnTableauBord.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTableauBord.Location = new System.Drawing.Point(0, 49);
+            this.btnTableauBord.Name = "btnTableauBord";
+            this.btnTableauBord.Size = new System.Drawing.Size(300, 50);
+            this.btnTableauBord.TabIndex = 6;
+            this.btnTableauBord.Text = "   Tableau de bord";
+            this.btnTableauBord.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnTableauBord.UseVisualStyleBackColor = false;
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnQuitter.Controls.Add(this.label6);
+            this.btnQuitter.Controls.Add(this.pictureBox6);
+            this.btnQuitter.Location = new System.Drawing.Point(0, 631);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(301, 50);
+            this.btnQuitter.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AllowDrop = true;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(56, 11);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 24);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Quitter";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(7, 0);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Padding = new System.Windows.Forms.Padding(2);
+            this.pictureBox6.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 0;
+            this.pictureBox6.TabStop = false;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(335, 29);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(64, 13);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Tableu bord";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(473, 210);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(39, 13);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "Engins";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(494, 376);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(61, 13);
+            this.lbl5.TabIndex = 3;
+            this.lbl5.Text = "Statistiques";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(458, 137);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(86, 13);
+            this.lbl2.TabIndex = 4;
+            this.lbl2.Text = "Nouvelle mission";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(473, 275);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(54, 13);
+            this.lbl4.TabIndex = 5;
+            this.lbl4.Text = "Humaines";
+            // 
+            // mainLayout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(235)))));
+            this.ClientSize = new System.Drawing.Size(1007, 749);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.pnlSideBar);
+            this.Name = "mainLayout";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " Les soldats du feu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.mainLayout_Load);
+            this.pnlSideBar.ResumeLayout(false);
+            this.pnlSideBar.PerformLayout();
+            this.btnQuitter.ResumeLayout(false);
+            this.btnQuitter.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSideBar;
+        private System.Windows.Forms.Panel btnQuitter;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Button btnTableauBord;
+        private System.Windows.Forms.Button btnGestionEngins;
+        private System.Windows.Forms.Button btnGestionPersonnel;
+        private System.Windows.Forms.Button btnStatistiques;
+        private System.Windows.Forms.Button btnNouvelleMission;
+    }
+}
+
---
+++ Barseghian_Nezami_SAE25/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Barseghian_Nezami_SAE25
+{
+    public partial class mainLayout : Form
+    {
+        public mainLayout()
+        {
+            InitializeComponent();
+        }
+
+        private void mainLayout_Load(object sender, EventArgs e)
+        {
+            // Button DashBoard
+            Image img = Image.FromFile(@"..\..\Resources\Icons\dashboard.png");
+            Image resizedImage = new Bitmap(img, new Size(50, 50)); 
+            btnTableauBord.Image = resizedImage;
+
+            // Button Nouvelle Mission
+            img = Image.FromFile(@"..\..\Resources\Icons\redalert.png");
+            resizedImage = new Bitmap(img, new Size(50, 50));
+            btnNouvelleMission.Image = resizedImage;
+
+            // Button Gestion Personnel
+            img = Image.FromFile(@"..\..\Resources\Icons\firefighter.png");
+            resizedImage = new Bitmap(img, new Size(50, 50));
+            btnGestionPersonnel.Image = resizedImage;
+
+            // Button Gestion des Engins
+            img = Image.FromFile(@"..\..\Resources\Icons\fireTruck.png");
+            resizedImage = new Bitmap(img, new Size(50, 50));
+            btnGestionEngins.Image = resizedImage;
+
+            // Button Statistiques
+            img = Image.FromFile(@"..\..\Resources\Icons\statistics.png");
+            resizedImage = new Bitmap(img, new Size(50, 50));
+            btnStatistiques.Image = resizedImage;
+        }
+        /***************************************************************************/
+      
+        /***************************************************************************/
+
+    }
+}
